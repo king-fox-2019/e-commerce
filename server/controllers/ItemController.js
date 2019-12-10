392e51ff843aa57355dbe6da7aa2b76d3db9a9cc
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-12-10 10:04:56 +0700

    Add get one item test - Add controller and routes for get onr item

--- server/controllers/ItemController.js
+++ server/controllers/ItemController.js
@@ -1,10 +1,19 @@
 const { Item } = require('../models')
-
+const createError = require('http-errors')
 class ItemController {
   static getAllItems(req, res, next) {
     Item.find()
       .then(items => {
         res.status(200).json({ data: items })
+      })
+      .catch(next)
+  }
+
+  static getOneItem(req, res, next) {
+    Item.findById(req.params.id)
+      .then(item => {
+        if (item) res.status(200).json({ data: item })
+        else throw createError(404, 'Item not found')
       })
       .catch(next)
   }
--- server/routes/items.js
+++ server/routes/items.js
@@ -2,5 +2,6 @@ const items = require('express').Router()
 const { ItemController } = require('../controllers')
 
 items.get('/', ItemController.getAllItems)
+items.get('/:id', ItemController.getOneItem)
 
 module.exports = items
