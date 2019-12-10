aa29d6932c48be1479e648d86d6905c278d9ac57
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-12-11 03:18:53 +0700

    Add main landing section - Modify Home.vue - Change some theme colors - Delete HelloWorld.vue - Almost done creating Jumbotron - Add ItemList.vue

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="accent">
+  <b-navbar toggleable="lg" type="dark" variant="accent" fixed>
     <b-navbar-brand to="/">LaZaloPedia</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
