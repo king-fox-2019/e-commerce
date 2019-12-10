54953015afd57a5e9ff63aa7e0ff42bc94348bcb
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-12-10 16:26:17 +0700

    Modify cart items test fucking done - Gosh it's so damn hard - First of all, the test itself is hard to create - Second, the transaction... This shit dont go well in mongoose and hard to get where the fuck is wrong, when something really gets wrong - And another shit to take care is the goddamn subdoc. Apparently this little guy has it's own id.. but why? BUT WHY? So I surf the net to find out if I could slap this id out of the subdoc schema. Thank god I found the solution. - That 3 dumbass shit took me back and forth checking Controller, Model, and even test file if there's any potential error between them - And now it's fucking done. And I fuckin done with this shit. Will never look back and check it out. EVER

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,6 @@ const { UserController, CartController } = require('../controllers')
 
 user.get('/checksession', UserController.checkSession)
 user.get('/cart', CartController.getUserCart)
+user.patch('/cart', CartController.updateUserCart)
 
 module.exports = user
