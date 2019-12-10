71fcb210dc44b36e044ccb7360ace6bdecee4f07
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-12-11 02:10:00 +0700

    Starting with Navbar - Install bootstrap and bootstrap-vue as css framework - Modify router linkActive default class to 'active' - Move router out of folder - Modify App.vue to show Navbar - Add some dummy router to check Navbar router-link - Add custom css to override default bootstrap-vue theme

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="accent">
+    <b-navbar-brand to="/">LaZaloPedia</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto" v-if="onSession">
+        <b-nav-item to="/signup">Cart</b-nav-item>
+        <b-nav-item to="/signin">Sign Out</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto" v-else>
+        <b-nav-item to="/signup">Sign Up</b-nav-item>
+        <b-nav-item to="/signin">Sign In</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {}
+  },
+  computed: {
+    onSession() {
+      return this.$store.state.onSession
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ client/src/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '@/views/Signup.vue')
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'active',
+  routes
+})
+
+export default router
