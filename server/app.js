a7471105d98d0f1beca0e4a5583148f015a52d0b
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-12-10 23:00:59 +0700

    Finishing for development env - Installing morgan for dev dependency - Installing cors for cross browser dependency - Add bin/http.js to start http server on default port 3000 - Modify app.js to add installed dependency

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
-if (process.env.NODE_ENV === 'developlent' || process.env.NODE_ENV === 'test')
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')
   require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 require('./config/mongoose')
 
+if (process.env.NODE_ENV === 'development') app.use(require('morgan')('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
