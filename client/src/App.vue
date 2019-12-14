9f8929a4d63811526f70b951bc034c1cbb1753ff
Amil Hasbala <amilhasbala@gmail.com>
2019-12-14 22:31:08 +0700

    tampilan client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <navbar class="navbar" @set-login="setLogin"></navbar>
+    <router-view @set-login="setLogin"/>
   </div>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  },
+  methods: {
+    setLogin: function () {
+      console.log('login emit dari children')
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.navbar {
+  width: 100vw;
+  z-index: 999;
+  position: fixed;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+router-view {
+  margin-top: 80px
 }
 </style>
---
+++ client/src/components/ProductDetail.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="productDetail">
+    <div id="productMain" class="row">
+      <div class="col-md-6">
+        <!-- <div data-slider-id="1" class="owl-carousel shop-detail-carousel owl-loaded owl-drag">
+          <div class="owl-stage-outer">
+            <div
+              class="owl-stage"
+              style="transform: translate3d(-1590px, 0px, 0px); transition: all 0.25s ease 0s; width: 2783px;"
+            >
+              <div class="owl-item cloned" style="width: 397.5px;">
+                <div class="item">
+                  <img src="img/detailbig2.jpg" alt class="img-fluid" />
+                </div>
+              </div>
+              <div class="owl-item cloned" style="width: 397.5px;">
+                <div class="item">
+                  <img src="img/detailbig3.jpg" alt class="img-fluid" />
+                </div>
+              </div>
+              <div class="owl-item" style="width: 397.5px;">
+                <div class="item">
+                  <img src="img/detailbig1.jpg" alt class="img-fluid" />
+                </div>
+              </div>
+              <div class="owl-item" style="width: 397.5px;">
+                <div class="item">
+                  <img src="img/detailbig2.jpg" alt class="img-fluid" />
+                </div>
+              </div>
+              <div class="owl-item active" style="width: 397.5px;">
+                <div class="item">
+                  <img src="img/detailbig3.jpg" alt class="img-fluid" />
+                </div>
+              </div>
+              <div class="owl-item cloned" style="width: 397.5px;">
+                <div class="item">
+                  <img src="img/detailbig1.jpg" alt class="img-fluid" />
+                </div>
+              </div>
+              <div class="owl-item cloned" style="width: 397.5px;">
+                <div class="item">
+                  <img src="img/detailbig2.jpg" alt class="img-fluid" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="owl-nav disabled">
+            <button type="button" role="presentation" class="owl-prev">
+              <span aria-label="Previous">‹</span>
+            </button>
+            <button type="button" role="presentation" class="owl-next">
+              <span aria-label="Next">›</span>
+            </button>
+          </div>
+          <div class="owl-dots disabled"></div>
+        </div> -->
+        <img src="https://images.unsplash.com/photo-1574327694472-d172fb4d5865?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1532&q=80" class="img-fluid" alt="">
+        <div class="ribbon sale">
+          <div class="theribbon">SALE</div>
+          <div class="ribbon-background"></div>
+        </div>
+        <!-- /.ribbon-->
+        <div class="ribbon new">
+          <div class="theribbon">NEW</div>
+          <div class="ribbon-background"></div>
+        </div>
+        <!-- /.ribbon-->
+      </div>
+      <div class="col-md-6">
+        <div class="box">
+          <h1 class="text-center">White Blouse Armani</h1>
+          <p class="goToDescription">
+            <a
+              href="#details"
+              class="scroll-to"
+            >Scroll to product details, material &amp; care and sizing</a>
+          </p>
+          <p class="price">$124.00</p>
+          <p class="text-center buttons">
+            <a href="basket.html" class="btn btn-primary">
+              <i class="fa fa-shopping-cart"></i> Add to cart
+            </a>
+          </p>
+        </div>
+        <div class="box" id="details">
+          <p>product detail bla bla</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailProduct'
+}
+</script>
+
+<style scoped>
+.productDetail {
+  padding: 2rem
+}
+</style>
