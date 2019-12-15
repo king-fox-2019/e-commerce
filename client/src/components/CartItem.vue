e02675c097216bacf177e25daac9840d841fe90f
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-12-15 11:36:44 +0700

    adding cart product page in client side

---
+++ client/src/components/CartItem.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <b-card class="card">
+      <b-card-text class="title-card">
+        Total Price
+      </b-card-text>
+      <hr>
+      <div>
+        <div class="row">
+          <b-card-text class="col-5">Item Price</b-card-text>
+          <div class="col-7 value">Rp. {{ this.item.toLocaleString("id") }}</div>
+          <div class="w-100"></div>
+          <b-card-text class="col col-5">Tax</b-card-text>
+          <div class="col-7 value">Rp. {{ tax.toLocaleString("id") }}</div>
+          <div class="w-100"></div>
+          <b-card-text class="col-5">Total Price</b-card-text>
+          <div class="col-7 value"> <strong> Rp. {{ total.toLocaleString("id") }} </strong></div>
+        </div>
+      </div>
+      <hr>
+      <b-button variant="success" class="addToCart"><i class="far fa-money-bill-alt mr-2" right></i>
+      <strong> Add To Cart</strong></b-button>
+    </b-card>
+    <b-card class="card-2 mt-4">
+      <b-card-text class="title-card-2">
+        PPh 22, Income Tax Article 22 for Gold Bars
+      </b-card-text>
+      <b-card-text>According to PMK No 34/PMK.10/2017, purchase of gold bars is subject to
+        a fee in accordance to PPh Art. 22 of 0,45% (for NPWP holders and 0,9%
+        for non-NPWP holders). Every purchase of gold bars is accompanied by
+        proof of deduction of PPh 22.</b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      item: 0,
+    };
+  },
+  computed: {
+    tax() {
+      return this.item * 10 / 100;
+    },
+    total() {
+      return this.item + this.tax;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 8px !important;
+  border: none;
+  text-align: left;
+}
+.title-card {
+  font-weight: bold;
+  font-size: 24px
+}
+
+.card-2 {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 8px !important;
+  border: none;
+  text-align: justify;
+  font-size: 12px
+}
+
+.title-card-2 {
+  font-weight: bold;
+}
+
+.value {
+  text-align: right;
+  width: 80%;
+}
+
+.row {
+  font-size: 16px;
+}
+
+.addToCart {
+  height: 50px;
+  width: 100% ;
+  font-size: 18px;
+}
+
+</style>
--- client/src/views/Product.vue
+++ client/src/views/Product.vue
@@ -5,7 +5,18 @@
     class="img-fluid header" alt="Responsive image">
     <div class="border-up"></div>
     <div class="d-flex p-2 bd-highlight header-item align-items-center">
-      <h2>Beli Emas</h2>
+      <div class="d-flex flex-column bd-highlight mb-3 desc-title">
+        <h1>Buy Gold</h1>
+        <p>ANTAM LM gold bars are guaranteed authenticity and purity with LBMA
+          (London Bullion Market Association) certificate. Immediately have
+          LM ANTAM gold bars ranging from 0.5 gram fractions to 1000 gram fractions.</p>
+      </div>
+    </div>
+    <div class="d-flex bd-highlight">
+      <div class="p-2 flex-grow-1 bd-highlight p-4">Flex item</div>
+      <div class="p-2 bd-highlight cart-item p-4">
+        <CartItem />
+      </div>
     </div>
     <Footer />
   </div>
@@ -14,10 +25,11 @@
 <script>
 import Navbar from '../components/Navbar.vue';
 import Footer from '../components/Footer.vue';
+import CartItem from '../components/CartItem.vue';
 
 export default {
   components: {
-    Navbar, Footer,
+    Navbar, Footer, CartItem,
   },
 };
 </script>
@@ -33,12 +45,27 @@ export default {
 }
 
 .header-item {
-  height: 200px;
+  height: 200px !important;
 }
 
-h2 {
-  font-weight: bold
+h1 {
+  font-weight: bold;
+  color: #cb9b2d;
+  margin-bottom: 10px
 }
 
+.cart-item {
+  width: 35% !important
+}
+
+.desc-title {
+  text-align: left;
+  padding-left: 30px;
+  padding-right: 30px
+}
+
+.mb-3 {
+  margin-bottom: 0 !important
+}
 
 </style>
