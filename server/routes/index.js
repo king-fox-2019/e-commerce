a5300472b2bc23c059de58baa16a5a9d3a802914
HSBL <46879920+HSBL@users.noreply.github.com>
2019-12-15 23:39:51 +0700

    Delete index.js

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router(),
-  userRoutes = require('./user'),
-  productRoutes = require('./product')
-
-router.use('/user', userRoutes)
-router.use('/products', productRoutes)
-
-module.exports = router
