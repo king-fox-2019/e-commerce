aaaff6b9c0e18e69feada44504e4047328858245
HSBL <46879920+HSBL@users.noreply.github.com>
2019-12-15 23:40:06 +0700

    Delete product.js

--- server/routes/product.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router(),
-  ProductController = require('../controllers/product')
-
-router.get('/', ProductController.all)
-router.post('/create', ProductController.create)
-router.delete('/:id', ProductController.delete)
-
-module.exports = router
