5c39ded05fcedd4689655989a33ef985dd0fbd46
HSBL <amilhasbala@gmail.com>
2019-12-15 10:11:57 +0000

    changes from iowa server

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@ if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
 
 const express = require('express'),
   app = express(),
+  cors = require('cors'),
   router = require('./routes'),
   errorHandler = require('./middlewares/errorHandler')
 
 require('./config/mongoose')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/', router)
--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 function generateToken(payload) {
-  return jwt.sign(payload, process.env.JWT_SECRET)
+  return jwt.sign(payload, 'secret')
 }
 
 function decodeToken(token){
-  return jwt.verify(token, process.env.JWT_SECRET)
+  return jwt.verify(token, 'secret')
 }
 
 module.exports = { generateToken, decodeToken }
