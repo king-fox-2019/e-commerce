bdb12a600f47639b65ff7489c7c757b8239bf25e
anggabanny <anggabanny@gmail.com>
2019-12-16 20:01:37 +0700

    deploy

--- client/apis/axios.js
+++ client/apis/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+// const instance = axios.create({
+//     baseURL: 'http://localhost:3000'
+// });
+
+// deploy
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://minipc-sportstation.anggabanny.online'
 });
 
 export default instance
