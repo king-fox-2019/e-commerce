95ba6d093ba57f2b2fa1f1046ab1dd34cfed7190
risanto <potallegta@gmail.com>
2019-12-16 15:15:56 +0700

    Fixed minor problems

--- client/config/axios.js
+++ client/config/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const axiosConfig = axios.create({
   baseURL: 'http://ec2-3-0-57-132.ap-southeast-1.compute.amazonaws.com'
+  // baseURL: 'http://localhost:3000'
 })
 
 export default axiosConfig
