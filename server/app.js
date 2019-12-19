7af155753bc6d3550d3674b22f18c714ec356015
risanto <potallegta@gmail.com>
2019-12-19 12:21:07 +0700

    MONGO_URI

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const mongoURI = process.env.MONGO_URI
 
 const app = express()
 
-let port = process.env.PORT_TEST || 3000
+let port = process.env.PORT || 3000
 
 // if (process.env.NODE_ENV === 'testing') {
 //   port = 4000
