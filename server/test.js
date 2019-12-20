f789a1a11443780926e2a98e2281f25d27eb410d
renault dzaky <dzakymon8@gmail.com>
2019-12-20 10:13:15 +0700

    add test for product and transaction

--- server/test.js
+++ server/test.js
@@ -13,7 +13,7 @@ describe('routes product', function(){
                 .send({ email: 'tester@mail.com', name: 'tester', password: 'tester' })
                 .end(function(req,res){
                     expect(res).to.have.status(201);
-                    expect(response.body).to.have.property('access_token');
+                    expect(res.body).to.have.property('access_token');
                     done()
                 })
         })
@@ -25,7 +25,7 @@ describe('routes product', function(){
                 .send({ email: 'tester@mail.com', password: 'tester' })
                 .end(function(req,res){
                     expect(res).to.have.status(200);
-                    expect(response.body).to.have.property('access_token');
+                    expect(res.body).to.have.property('access_token');
                     access_token = res.body.access_token
                     done()
                 })
@@ -38,6 +38,7 @@ describe('routes product', function(){
                 .set('access_token', access_token)
                 .end(function(req,res){
                     expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
                     done()
                 })
         })
@@ -50,6 +51,7 @@ describe('routes product', function(){
                 .send({ product: product_id, amount: 2 })
                 .end(function(req,res){
                     expect(res).to.have.status(200);
+                    expect(res.body.nModified).to.equal(1)
                     done()
                 })
         })
@@ -62,6 +64,7 @@ describe('routes product', function(){
                 .send({ product: product_id })
                 .end(function(req,res){
                     expect(res).to.have.status(200);
+                    expect(res.body.nModified).to.equal(1)
                     done()
                 })
         })
@@ -74,6 +77,7 @@ describe('routes product', function(){
                 .send({ product: product_id })
                 .end(function(req,res){
                     expect(res).to.have.status(200);
+                    expect(res.body.nModified).to.equal(1)
                     done()
                 })
         })
@@ -85,6 +89,55 @@ describe('routes product', function(){
                 .set('access_token', access_token)
                 .end(function(req,res){
                     expect(res).to.have.status(200);
+                    expect(res.body.n).to.equal(1)
+                    done()
+                })
+        })
+    })
+    describe('get list of product', function(){
+        it('user should able to fetch product', function(done){
+            chai.request(app)
+                .get(`/product`)
+                .set('access_token', access_token)
+                .end(function(req,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    expect(res.body[0]).to.have.property('_id');
+                    expect(res.body[0]).to.have.property('name');
+                    expect(res.body[0]).to.have.property('price');
+                    expect(res.body[0]).to.have.property('description');
+                    expect(res.body[0]).to.have.property('image');
+                    expect(res.body[0]).to.have.property('stock');
+                    done()
+                })
+        })
+    })
+    describe('get detail of product', function(){
+        it('user should able to fetch one product', function(done){
+            chai.request(app)
+                .get(`/product/${product_id}`)
+                .set('access_token', access_token)
+                .end(function(req,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body).to.have.property('_id');
+                    expect(res.body).to.have.property('name');
+                    expect(res.body).to.have.property('price');
+                    expect(res.body).to.have.property('description');
+                    expect(res.body).to.have.property('image');
+                    expect(res.body).to.have.property('stock');
+                    done()
+                })
+        })
+    })
+    describe('get user transaction', function(){
+        it('user should able to fetch their own transaction list', function(done){
+            chai.request(app)
+                .get(`/transaction/user`)
+                .set('access_token', access_token)
+                .end(function(req,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
                     done()
                 })
         })
